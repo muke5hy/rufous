91a653b7e5a63501e4a2719a4b9cbf94a454da25
Mukesh Yadav <mak.gnu@gmail.com>
2019-12-04 09:39:45 +0530

    Registratiion

---
+++ users/authentication.py
@@ -0,0 +1,18 @@
+from drf_firebase.authentication import BaseFirebaseAuthentication
+from firebase_admin import credentials, initialize_app
+from django.contrib.auth import get_user_model
+
+firebase_creds = credentials.Certificate('rufous/firebase/credentials.json')
+firebase_app = initialize_app(firebase_creds)
+
+class FirebaseAuthentication(BaseFirebaseAuthentication):
+	"""
+	Example implementation of a DRF Firebase Authentication backend class
+	"""
+	def get_firebase_app(self):
+		return firebase_app
+
+	def get_django_user(self, firebase_user_record):
+		return get_user_model().objects.get_or_create(
+			username=firebase_user_record.uid,
+		)[0]
